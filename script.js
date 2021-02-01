78eee2667c3a5ea444f811401e8daf2b20759c63
DimaVnuk <vnuchok311994@hotmail.com>
2021-02-01 19:26:06 +0300

    add  space

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let nameValue = prompt('Enter your name');
 let surnameValue = prompt('Enter your surname');
 let nicknameValue = prompt('Enter your nickname');
 if(nameValue && surnameValue && nicknameValue){
-alert('Hello ' + nameValue + surnameValue);
+alert('Hello ' + nameValue + ' ' + ' ' + surnameValue);
 }
 else{
     alert('Hello noname');
