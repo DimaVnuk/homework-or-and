7da3f45301efbd5210a7958b04b36a5feab3d9a4
DimaVnuk <vnuchok311994@hotmail.com>
2021-02-04 21:42:20 +0300

    add case 4

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-/* let nameValue = prompt('Enter your name');
+let nameValue = prompt('Enter your name');
 let surnameValue = prompt('Enter your surname');
 let nicknameValue = prompt('Enter your nickname');
 if (nameValue && surnameValue && nicknameValue) {
@@ -14,10 +14,10 @@ else if (nameValue && nicknameValue) {
     alert('Hello ' + nameValue);
 } else {
     alert('Hello Noname');
-} */
-/* end case 1 */
+}
+/* case 1 */
 
-/* let numberValue = +prompt('Enter your number');
+let numberValue = +prompt('Enter your number');
 let numberEven = numberValue % 2 == 0;
 let numberOdd = numberValue % 2 !== 0;
 let numberPositive = numberValue > 0; 
@@ -65,19 +65,48 @@ else if(numberOdd  && numberPositive && numberFractional && numberIsMoreHundred)
 }
 else {
     alert('OOOPS, its a BUG')
-} */
+}
 /* 2 case */
 
 
-/* let valueNumberFirst = prompt('enter your number');
+let valueNumberFirst = prompt('enter your number');
 let valueNumberSecond = prompt('enter your second number');
 let valueNumberThird = prompt('enter your third number');
 let message = valueNumberFirst ? alert(Number(valueNumberFirst) + Number(valueNumberSecond) + Number(valueNumberThird)):
-alert('Bye'); */
-
+alert('Bye');
 /* case 3.1 */
 
-/* let nameValue = prompt('enter your name');
+let nameValue = prompt('enter your name');
 let age = prompt('enter your age');
-let alertMessage = (age > 17) ? alert('Welcome ' + nameValue) : alert('Sorry ' + nameValue + ' access is closed'); */
+let alertMessage = (age > 17) ? alert('Welcome ' + nameValue) : alert('Sorry ' + nameValue + ' access is closed');
 /* case 3.2 */
+
+let numberUserValue = prompt('Enter your number');
+switch (numberUserValue || isNaN(numberUserValue)) {
+    case 0:
+        alert('zero');
+    case 1:
+        alert('one');
+    case 2:
+        alert('two');
+    case 3:
+        alert('three');
+    case 4:
+        alert('four');
+    case 5:
+        alert('five');
+    case 6:
+        alert('six');
+    case 7:
+        alert('seven');
+    case 8:
+        alert('eight');
+    case 9:
+        alert('nine');
+        break
+    default:
+        alert('too much')
+}
+/* case 4 */
+
+
