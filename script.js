e6274c32f206bdd9bc4200ae2bef17531216d10b
DimaVnuk <71352399+DimaVnuk@users.noreply.github.com>
2021-02-28 15:18:54 +0300

    Update script.js

--- script.js
+++ script.js
@@ -90,22 +90,31 @@ if (isNaN(numberUserValuePrompt)) {
     switch (numberUserValuePrompt) {
         case 0:
             alert('zero');
+            break
         case 1:
             alert('one');
+            break
         case 2:
             alert('two');
+            break
         case 3:
             alert('three');
+            break
         case 4:
             alert('four');
+            break
         case 5:
             alert('five');
+            break
         case 6:
             alert('six');
+            break
         case 7:
             alert('seven');
+            break
         case 8:
             alert('eight');
+            break
         case 9:
             alert('nine');
             break
